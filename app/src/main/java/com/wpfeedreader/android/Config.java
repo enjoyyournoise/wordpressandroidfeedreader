d1990292c5a93fb7d28eb0d5df44cd081381415c
Diego Gurnari <diego@cityglance.it>
2017-09-16 10:54:15 +0200

    :sparkles: Initial Commit

---
+++ app/src/main/java/com/wpfeedreader/android/Config.java
@@ -0,0 +1,10 @@
+package com.wpfeedreader.android;
+
+/**
+ * Created by Diego on 06/09/2017.
+ */
+
+public class Config {
+
+    public static String URL_POSTS = "https://www.agentianonimi.com/wp-json/wp/v2/posts?_embed=true&filter[posts_per_page]=50&fields=id,title";
+}
